1fd577afb775f02c8f7f889e8ca63022cc26a6d0
Halász Benjamin <47182317+HuserB8927@users.noreply.github.com>
2020-05-08 15:11:18 +0200

    login page out for now

--- src/main/java/com/benjaminhalasz/config/SecurityConf.java
+++ src/main/java/com/benjaminhalasz/config/SecurityConf.java
@@ -71,6 +71,7 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 		try {
 			httpSec.authorizeRequests()
 			.antMatchers("/sw.js").permitAll()
+			.antMatchers("/pdf/benjamin.pdf").permitAll()
 			.antMatchers("/").permitAll()
 				.antMatchers(staticResources).permitAll()
 				.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
